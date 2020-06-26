17f49d8119fc9297046723d923ba6a42408a954c
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2020-06-26 04:55:30 +0200

    Initial commit

---
+++ HChat/HChat.h
@@ -0,0 +1,19 @@
+//
+//  HChat.h
+//  HChat
+//
+//  Created by Hassan Khamis on 6/26/20.
+//  Copyright © 2020 Hassan Khamis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HChat.
+FOUNDATION_EXPORT double HChatVersionNumber;
+
+//! Project version string for HChat.
+FOUNDATION_EXPORT const unsigned char HChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HChat/PublicHeader.h>
+
+
